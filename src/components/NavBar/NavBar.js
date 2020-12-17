947ecdd05ca9ce62e77c49383fa4bed6620bbc0d
Maximusvin <Maximusvin@gmail.com>
2020-12-17 10:30:49 +0200

    Добавил изменение фона для меню при прокрутке

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { AiOutlineUserAdd, AiOutlineClockCircle } from 'react-icons/ai';
 import Layout from '../Layout/Layout';
 import logoImg from '../../images/burger.svg';
@@ -5,9 +6,24 @@ import Buttom from '../../UI/IconButton/IconButton';
 import s from './NavBar.module.css';
 
 const NavBar = () => {
+  const [nav, setNav] = useState(s.nav);
+
+  const listenScrollEvent = () => {
+    if (window.scrollY < 100) {
+      return setNav(s.nav);
+    } else if (window.scrollY > 90) {
+      return setNav(s.navBlack);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+    return () => window.removeEventListener('scroll', listenScrollEvent);
+  }, []);
+
   return (
     <header className={s.header}>
-      <nav className={s.nav}>
+      <nav className={nav}>
         <div className={s.logoWrap}>
           <img className={s.logoImg} src={logoImg} alt="Logo VinBurger" />
           <h1 className={s.title}>VinBurger</h1>
@@ -18,7 +34,7 @@ const NavBar = () => {
           />
           График работы
         </div>
-        <a href="tel:0979769625" className={s.delivery}>
+        <a href="tel:380979769625" className={s.delivery}>
           <span>Доставка:</span>
           +3 8 (097) 97-696-25
         </a>
@@ -35,7 +51,7 @@ const NavBar = () => {
           <p className={s.description}>
             У каждого есть идеальная пара. Иногда приходится путешествовать по
             миру в её поисках. И где бы ни был ваш идеальный бургер, он всегда
-            будет рядом.
+            будет рядом вместе с VinBurger.
           </p>
           <a href="#menu" className={s.offerButton}>
             Меню
